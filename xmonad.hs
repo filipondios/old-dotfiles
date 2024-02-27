09d09e8f295b88dee8f92ff775adb4a97fb02048
dpv927 <dpv927@inlumine.ual.es>
2024-02-27 02:49:09 +0100

    Bar above xmonad but working

--- xmonad.hs
+++ xmonad.hs
@@ -4,7 +4,13 @@ import System.Exit
 import Graphics.X11.ExtraTypes.XF86
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.Spacing
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.UrgencyHook
 import XMonad.Util.SpawnOnce
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, xmobarPP, xmobarColor, shorten, PP(..))
+import XMonad.Util.Hacks (windowedFullscreenFixEventHook, javaHack, trayerAboveXmobarEventHook, trayAbovePanelEventHook, trayerPaddingXmobarEventHook, trayPaddingXmobarEventHook, trayPaddingEventHook)
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 import System.IO
@@ -54,19 +60,20 @@ myStartupHook :: X ()
 myStartupHook = do
   spawn "~/.fehbg"   
   spawnOnce "picom"
+  setWMName "LG3D"
 
 -- Status bar
-myBar :: String
-myBar = "xmobar"
-barPP = xmobarPP { ppCurrent = xmobarColor "#1b1f24" "" . wrap "<" ">" }
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+-- myBar :: String
+-- myBar = "xmobar"
+-- barPP = xmobarPP { ppCurrent = xmobarColor "#1b1f24" "" . wrap "<" ">" }
+-- toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 -- Key Bindings
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [((modm, xK_Return), spawn $ XMonad.terminal conf)  -- Terminal
     ,((modm, xK_p), spawn (myAppLauncher))              -- Open application launcher	
     ,((modm, xK_f), spawn (myWebBrowser))               -- Open browser
-    ,((modm, xK_q), kill)			                          -- Close window
+    ,((modm, xK_q), kill)                               -- Close window
     ,((modm, xK_s), windows W.swapMaster) 		          -- Swap focused and master
     ,((modm, xK_d), sendMessage Shrink)                 -- Shrink master area
     ,((modm, xK_i), sendMessage Expand)			            -- Expand master area
@@ -86,19 +93,53 @@ myLayoutHook = spacing windowGaps $ tiled |||  Mirror tiled ||| Full
     ratio = 1/2
     delta = 3/100
 
--- Main config
-main = xmonad =<< statusBar myBar barPP toggleStrutsKey defaults
+-- Workspace window count function
+windowCount :: X (Maybe String)
+windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
-defaults = def {
-    terminal           = myTerminal,
-    focusFollowsMouse  = canMouseFocus,
-    clickJustFocuses   = canClickFocus,
-    borderWidth        = myWindowBorder,
-    modMask            = myModMask,
-    layoutHook         = myLayoutHook,
-    workspaces         = myWorkspaces,
-    normalBorderColor  = inactiveWindowBorder,
-    focusedBorderColor = focusedWindowBorder,
-    startupHook        = myStartupHook,
-    keys               = myKeys
-}
+main = do
+  xmproc <- spawnPipe "xmobar /home/tux/.config/xmonad/xmobar"
+  xmonad $ def 
+    {
+      -- Simple stuff
+      terminal           = myTerminal,
+      focusFollowsMouse  = canMouseFocus,
+      borderWidth        = myWindowBorder,
+      modMask            = myModMask,
+      workspaces         = myWorkspaces,
+      normalBorderColor  = inactiveWindowBorder,
+      focusedBorderColor = focusedWindowBorder,
+      keys = myKeys,
+
+      -- Hooks, Layouts, ...
+      -- manageHook = manageDocks <+> manageHook defaultConfig,
+      layoutHook = avoidStruts $ myLayoutHook,
+      -- handleEventHook = handleEventHook -- <+> docksEventHook defaultConfig,
+
+      logHook = dynamicLogWithPP xmobarPP
+        { ppOutput = hPutStrLn xmproc,
+          -- Current workspace
+          ppCurrent = xmobarColor "#adadff" "" . wrap 
+            ("<box type=Bottom width=2 mb=2 color=#adadff>") "</box>",
+          -- Visible but not current workspace
+          ppVisible = xmobarColor "#adadff" "",
+          -- Hidden workspace
+          ppHidden = xmobarColor "#b39692" "", -- . wrap
+          -- ("<box type=Top width=2 mt=2 color=#ffd5cd>") "</box>",
+          -- Hidden workspaces (no windows)
+          ppHiddenNoWindows = xmobarColor "#ffd5cd" "",
+          -- Title of active window
+          ppTitle = xmobarColor "#8f9ba6" "" . shorten 60,
+          -- Separator character
+          ppSep =  "<fc=#484c5b> <fn=1>|</fn> </fc>",
+          -- Urgent workspace
+          ppUrgent = xmobarColor "#c47eb7" "" . wrap "!" "!",
+          -- Adding # of windows on current workspace to the bar
+          ppExtras  = [windowCount],
+          -- order of things in xmobar
+          ppOrder  = \(ws:l:t:ex) -> [ws,l]++ex++[t]
+        },
+      startupHook = myStartupHook
+    }
+
+
