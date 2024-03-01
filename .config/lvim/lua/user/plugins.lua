c399a0d9e57fc531a013a88e79947a6b21a7b392
dpv927 <dpv927@inlumine.ual.es>
2024-03-01 01:11:12 +0100

    delete lvim

--- .config/lvim/lua/user/plugins.lua
+++
@@ -1,7 +0,0 @@
--- Define lvim list of plugins.
-lvim.plugins = {
-  {'kwsp/halcyon-neovim'},
-  {'andweeb/presence.nvim'},
-  {'norcalli/nvim-colorizer.lua'},
-  {'barrett-ruth/live-server.nvim'}
-}
