26a411b9ec92807fdd378e4e01563adb389df3e6
dpv927 <dpv927@inlumine.ual.es>
2024-09-28 03:52:46 +0200

    Change dashboard and statusline

--- .config/nvim/after/plugin/dashboard.lua
+++ .config/nvim/after/plugin/dashboard.lua
@@ -9,25 +9,33 @@ end
 
 -- Load a custom dashboard
 -- Art from:
--- https://github.com/MaximilianLloyd/ascii.nvim/blob/master/lua/ascii/misc/hydra.lua
+-- https://github.com/MaximilianLloyd/ascii.nvim/blob/master/lua/ascii/planets/planets.lua
 local dashboard = require('alpha.themes.dashboard')
 dashboard.section.header.opts.hl = 'DashboardHeader'
 dashboard.section.header.val = {
-	"                                   ",
-	"                                   ",
-	"                                   ",
-	"   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
-	"    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
-	"          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
-	"           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
-	"          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
-	"   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
-	"  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
-	" ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
-	" ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
-	"      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
-	"       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
-	"                                   ",
+  [[                                                   ]],
+  [[                                              ___  ]],
+  [[                                           ,o88888 ]],
+  [[                                        ,o8888888' ]],
+  [[                  ,:o:o:oooo.        ,8O88Pd8888"  ]],
+  [[              ,.::.::o:ooooOoOoO. ,oO8O8Pd888'"    ]],
+  [[            ,.:.::o:ooOoOoOO8O8OOo.8OOPd8O8O"      ]],
+  [[           , ..:.::o:ooOoOOOO8OOOOo.FdO8O8"        ]],
+  [[          , ..:.::o:ooOoOO8O888O8O,COCOO"          ]],
+  [[         , . ..:.::o:ooOoOOOO8OOOOCOCO"            ]],
+  [[          . ..:.::o:ooOoOoOO8O8OCCCC"o             ]],
+  [[             . ..:.::o:ooooOoCoCCC"o:o             ]],
+  [[             . ..:.::o:o:,cooooCo"oo:o:            ]],
+  [[          `   . . ..:.:cocoooo"'o:o:::'            ]],
+  [[          .`   . ..::ccccoc"'o:o:o:::'             ]],
+  [[         :.:.    ,c:cccc"':.:.:.:.:.'              ]],
+  [[       ..:.:"'`::::c:"'..:.:.:.:.:.'               ]],
+  [[     ...:.'.:.::::"'    . . . . .'                 ]],
+  [[    .. . ....:."' `   .  . . ''                    ]],
+  [[  . . . ...."'                                     ]],
+  [[  .. . ."'                                         ]],
+  [[ .                                                 ]],
+  [[                                                   ]],
 }
 
 -- Set a custom welcome message at the dashboard footer
